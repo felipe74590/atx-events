376cea3ddb8f7dddf1f0d7907dd91d91163974f8
Felipe Contreras <felipecontreras@Felipes-Laptop.attlocal.net>
2024-09-13 16:37:47 -0500

    finish adding second data source

--- db_helper.py
+++ db_helper.py
@@ -21,23 +21,25 @@ SQLModel.metadata.create_all(engine)
 
 
 def add_events_to_db(events):
-
+    events_added = 0
     with Session(engine) as session:
         # check if events already exsits based on title and start_datetime
         for event in events:
             existing_event = session.exec(
                 select(Event).where(
-                    Event.title == event["title"],
-                    Event.start_datetime == event["start_datetime"],
+                    Event.title == event.title,
+                    Event.start_datetime == event.start_datetime,
                 )
             ).first()
 
             if existing_event is not None:
                 new_event = Event(**event)
                 session.add(new_event)
+                events_added +=1
             else:
                 print(
-                    f"This event {event["title"]} on {event["start_datetime"]} already exists in database"
+                    f"This event {event.title} on {event.start_datetime} already exists in database"
                 )
+        print(f"A total of {events_added} Events were added to the database.")
 
         session.commit()
--- web_scraper.py
+++ web_scraper.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from typing import NamedTuple, Optional
 
 import requests
 from bs4 import BeautifulSoup
@@ -7,6 +8,14 @@ from db_helper import add_events_to_db
 
 SOURCE1 = "https://do512.com"
 SOURCE2 = "https://heyaustin.com/austin-events/"
+
+
+class Event(NamedTuple):
+    title: str
+    start_datetime: str
+    venue: str
+    category: Optional[str] = None
+    event_link: Optional[str] = None
 
 
 def get_page(page: str):
@@ -22,7 +31,7 @@ def get_page(page: str):
     return soup
 
 
-def gather_512_events_data(url: str) -> list:
+def gather_512_events_data(url: str, event_list=[]) -> list[Event]:
     """
     Gather important data from events in Do512 pages
     :param url: the current url of the page being scraped
@@ -30,37 +39,32 @@ def gather_512_events_data(url: str) -> list:
     hot_soup = get_page(url)
     events_soup = hot_soup.find_all("div", class_="ds-listing")
     next_page = hot_soup.find("a", class_="ds-next-page")
-
-    events = []
+    events = event_list
     for event in events_soup:
         event_details_links = SOURCE1 + event["data-permalink"]
         category_types = event["class"][2][9:]
 
         title = event.find("span", class_="ds-listing-event-title-text").text.strip()
-        event_date = event.find("meta", itemprop="startDate")["datetime"]
+        event_date = event.find("meta", itemprop="startDate")["datetime"][:-5]
+        start_time = datetime.strptime(event_date, '%Y-%m-%dT%H:%M')
         venue_details = event.find("div", class_="ds-venue-name")
         venue_location = venue_details.find("span", itemprop="name").text.strip()
-        this_event = {
-            "title": title,
-            "category": category_types,
-            "venue": venue_location,
-            "link": event_details_links,
-            "start_datetime": event_date,
-        }
+        this_event = Event(
+            title, start_time, venue_location, category_types, event_details_links,
+        )
 
         events.append(this_event)
 
     if next_page != None:
         next_page_url = next_page["href"]
         source = SOURCE1 + next_page_url
-        gather_512_events_data(source)
+        gather_512_events_data(source, events)
     else:
         print("No next page")
     return events
 
 
-
-def extract_details(details_url: str):
+def extract_details(details_url: str) ->Event:
     """
     Extract all details from subpage
     :param events_soup
@@ -68,23 +72,48 @@ def extract_details(details_url: str):
     hot_soup = get_page(details_url)
     event_soup = hot_soup.find("div", class_="fbecol-8-12")
     title = event_soup.h1.text.strip()
-    venue_details = hot_soup.find("div", class_="detail_items")
-    date = venue_details.div.span.text.strip()
-    ##TODO: Find out how to get the second div in venue_details which contains event start time and the third div in venue_details which contains venue name.
+    venue_details = hot_soup.find("div", class_="detail_items").find_all('div')[:4]
+    event_link = details_url
+    #Sometimes the venue is in index 2 other times, index 2 is a link for tickets and index 3 has the venue name
+    date, time, venue_maybe, venue_maybe2 = [detail.text.strip() for detail in venue_details]
+
+    if "Get Tickets" in venue_maybe: venue = venue_maybe2
+    else: venue = venue_maybe
+
+    #cleaning up the event start time to be a datetime object that matches those in the first event source
+    if "-" in time: 
+        start_time = time.split("-")
+        start_time = start_time[0].strip()
+    else: start_time = time
+    
+    datetime_str = f'{date} {start_time.upper()}'
+    start_datetime = datetime.strptime(datetime_str, '%b %d, %Y %I:%M %p')
+    
+    event = Event(
+        title,
+        start_datetime,
+        venue,
+        None,
+        event_link,
+    )
+    return event
 
 
-def gather_events_data_hey_austin(url: str):
+def gather_hey_austin_events_data(url: str, events_list=[]):
     """
     Gather important data from events in HeyAustin pages
     :param url: the current url of the page being scraped
     """
+    events = events_list
     hot_soup = get_page(url)
     events_soup = hot_soup.find_all("div", class_="fbe_col_title")
     for event in events_soup:
         event_details = extract_details(event.a["href"])
+        events.append(event_details)
+    return events
 
 
 if __name__ == "__main__":
-    data = gather_events_data_hey_austin(SOURCE2)
+    data = gather_hey_austin_events_data(SOURCE2)
     # data = gather_512_events_data(SOURCE1)
-    # add_events_to_db(data)
+    add_events_to_db(data)
