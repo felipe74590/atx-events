356d61a1d407c4bbc9c673f469414d7bf217aac9
Felipe Contreras <felipecontreras@Felipes-Laptop.local>
2024-10-06 22:41:55 -0700

    prep for deployment

--- src/data/db_helper.py
+++ src/data/db_helper.py
@@ -94,26 +94,27 @@ async def get_current_active_user(current_user: UserInDB = Depends(get_current_u
 
 
 ### ADDING EVENTS TO DB FROM WEBSCRAPERS
-def add_events_to_db(session, events):
+def add_events_to_db(events):
 	events_added = 0
 	# check if events already exsits based on title and start_datetime
-	for event in events:
-		existing_event = session.exec(
-			select(Event).where(
-				Event.title == event.title,
-				Event.start_datetime == event.start_datetime,
-			)
-		).first()
+	with Session(engine) as session:
+		for event in events:
+			existing_event = session.exec(
+				select(Event).where(
+					Event.title == event.title,
+					Event.start_datetime == event.start_datetime,
+				)
+			).first()
 
-		if existing_event is None:
-			new_event = Event(
-				**event._asdict(),
-			)
-			session.add(new_event)
-			events_added += 1
+			if existing_event is None:
+				new_event = Event(
+					**event._asdict(),
+				)
+				session.add(new_event)
+				events_added += 1
 
-		else:
-			print(f"This event {event.title} on {event.start_datetime} already exists in database")
-	print(f"A total of {events_added} Events were added to the database.")
+			else:
+				print(f"This event {event.title} on {event.start_datetime} already exists in database")
+		print(f"A total of {events_added} Events were added to the database.")
 
-	session.commit()
+		session.commit()
