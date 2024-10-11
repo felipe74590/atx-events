fb728bc94f748fb494cfc0c7590745c899c13062
Bob Belderbos <bobbelderbos@gmail.com>
2024-10-11 16:05:54 +0200

    rm db from version control, ignore it and updated main to run all sources by default

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import argparse
-from decouple import config
 from src.constants import SOURCE_ONE, SOURCE_THREE, SOURCE_TWO
 from src.data.db_helper import add_events_to_db
 from src.web_scrapping.web_scraper import (
@@ -7,8 +6,6 @@ from src.web_scrapping.web_scraper import (
     gather_events_data_source_do512,
     gather_events_data_source_heyaustin,
 )
-
-ENV = config("ENV")
 
 
 def handler_function():
@@ -20,17 +17,26 @@ def handler_function():
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-s", "--source", help="Enter source: source1, source2, source3")
+    parser.add_argument("-s", "--source", help="Enter source: source1, source2, source3, all")
     args = parser.parse_args()
 
     match args.source:
         case "source1":
             data = gather_events_data_source_do512(SOURCE_ONE)
+            add_events_to_db(data)
         case "source2":
             data = gather_events_data_source_heyaustin(SOURCE_TWO)
+            add_events_to_db(data)
         case "source3":
             data = gather_events_data_atx_culture(SOURCE_THREE)
+            add_events_to_db(data)
         case _:
-            print("Invalid input")
+            print("No source provided, run all")
+            for func, source in [
+                (gather_events_data_source_do512, SOURCE_ONE),
+                (gather_events_data_source_heyaustin, SOURCE_TWO),
+                (gather_events_data_atx_culture, SOURCE_THREE),
+            ]:
+                data = func(source)
+                add_events_to_db(data)
 
-    add_events_to_db(data)
