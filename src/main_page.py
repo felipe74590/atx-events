a8508f8acc0f8639d92999a1d2d841c0591eba9d
Felipe Contreras <felipecontreras@Felipes-Laptop.attlocal.net>
2024-10-14 01:09:31 -0500

    making it readable, using some pandas

--- src/main_page.py
+++ src/main_page.py
@@ -1,9 +1,12 @@
 import streamlit as st
 import requests
+import pandas as pd
+from datetime import datetime
 from constants import EVENTS_API_URL, MAX_EVENTS_LIMIT
 
 
-st.write("Local events")
+st.title("Events in Austin Texas")
+st.write("Here are some upcoming events:")
 
 
 def load_events(current_count=0, data=None):
@@ -24,4 +27,15 @@ def load_events(current_count=0, data=None):
 
 
 events = load_events()
+df = pd.DataFrame(events)
+
+
+for index, row in df.iterrows():
+    dt = datetime.fromisoformat(row["start_datetime"])
+    friendly_start_time = dt.strftime("%B %d, %Y, %I:%M %p")
+    st.markdown(f"**{row['title']}**")
+    st.markdown(f"Venue: {row['venue']}")
+    st.markdown(f"Starts:{friendly_start_time}")
+    st.markdown(f"[Event Link]({row["event_link"]})")
+    st.markdown("--------------------")
 st.write(events)
