eeb95d008a069993f49e7fb36707935148a894f9
Felipe Contreras <felipecontreras@Felipes-Laptop.attlocal.net>
2024-10-16 22:05:28 -0500

    added pagination

--- src/main_page.py
+++ src/main_page.py
@@ -5,24 +5,29 @@ from datetime import datetime
 from operator import itemgetter
 from constants import EVENTS_API_URL, MAX_EVENTS_LIMIT
 
-
+st.set_page_config(layout="centered")
 st.title("Events in Austin Texas")
 st.write("Here are some upcoming events:")
 
 
-def get_events(current_count=0, data=None) -> list:
+@st.cache_data(show_spinner=False)
+def get_events() -> list:
     """Loads events based on todays date"""
-    if not data:
-        data = []
+    current_count = 0
+    data = []
 
-    params = {"skip": current_count, "limit": MAX_EVENTS_LIMIT}
-    response = requests.get(url=EVENTS_API_URL + "/events", params=params)
-    response.raise_for_status()
+    while True:
+        params = {"skip": current_count, "limit": MAX_EVENTS_LIMIT}
+        response = requests.get(url=EVENTS_API_URL + "/events", params=params)
+        response.raise_for_status()
 
-    if len(response.json()) >= MAX_EVENTS_LIMIT:
-        current_count += 50
-        data.extend(response.json())
-        get_events(current_count, data)
+        events = response.json()
+
+        if not events:
+            break
+
+        current_count += MAX_EVENTS_LIMIT
+        data.extend(events)
 
     return data
 
@@ -39,14 +44,22 @@ def sort_events_by_time(events: list) -> list:
     return events
 
 
+# Load, sort, and convert events to DataFrame
 data = get_events()
 events = sort_events_by_time(data)
-
-
 df = pd.DataFrame(events)
 
+# Pagination controls
+total_events = len(df)
+events_per_page = 50
+total_pages = (total_events // events_per_page) + (total_events % events_per_page > 0)
 
-for index, row in df.iterrows():
+# Sidebar for pagination
+page_number = st.sidebar.number_input("Select Page:", 1, total_pages, 1)
+start_index = (page_number - 1) * events_per_page
+end_index = start_index + events_per_page
+
+for index, row in df.iloc[start_index:end_index].iterrows():
     dt = datetime.fromisoformat(row["start_datetime"])
     friendly_start_time = dt.strftime("%B %d, %Y, %I:%M %p")
     st.markdown(f"**{row['title']}**")
@@ -54,4 +67,3 @@ for index, row in df.iterrows():
     st.markdown(f"Starts:{friendly_start_time}")
     st.markdown(f"[Event Link]({row["event_link"]})")
     st.markdown("--------------------")
-st.write(events)
