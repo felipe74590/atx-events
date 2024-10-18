eb3f2faba5463bcff95df233387258de70a249f6
Felipe Contreras <felipecontreras@Felipes-Laptop.attlocal.net>
2024-10-18 07:09:59 -0500

    register user

--- src/pages/profile.py
+++ src/pages/profile.py
@@ -1,10 +1,19 @@
 import streamlit as st
+import requests
+from constants import EVENTS_API_URL
 
 st.write("My profile")
 
 
 def register_user(user_name, password, email):
     """Register a user into the database."""
+    data = {"user_name": user_name, "email": email, "password": password}
+    registration_response = requests.post(EVENTS_API_URL + "/users/", json=data)
+    if registration_response.status_code == 409:
+        st.error("User already exists.")
+    else:
+        st.success("User registered successfully.")
+    return registration_response.json()
 
 
 def login_user(user_name, password):
@@ -15,12 +24,18 @@ def login_user(user_name, password):
 tab1, tab2 = st.tabs(["Register", "Login"])
 with tab1:
     st.header("Register")
-    reg_username = st.text_input("Username", key="reg_username")
-    reg_password = st.text_input("Password", type="password", key="reg_password")
-    reg_email = st.text_input("Email")
+    if "registered" in st.session_state and st.session_state["registered"]:
+        st.success("Registration succesful, you can now login!")
+    else:
+        reg_username = st.text_input("Username", key="reg_username")
+        reg_password = st.text_input("Password", type="password", key="reg_password")
+        reg_email = st.text_input("Email")
 
-    if st.button("Register"):
-        register_user(reg_username, reg_password, reg_email)
+        if st.button("Register"):
+            if register_user(reg_username, reg_password, reg_email):
+                st.session_state["registered"] = True
+                st.experimental_rerun()
+
 
 with tab2:
     st.header("Login")
